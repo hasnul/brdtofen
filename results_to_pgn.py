3ac70dbb576186e1d2956e65f5d760d243e58e53
bob <bob@local.net>
2022-10-16 21:29:11 +0800

    Program for converting results to pgn

---
+++ results_to_pgn.py
@@ -0,0 +1,59 @@
+#!/bin/python3
+
+import argparse
+import json
+import chess
+
+
+def header(book_title, white, black, result, num, confidence, fen, pgn):
+    print(f'[Event "{book_title}"]', file=pgn)
+    print(f'[Site "Book"]', file=pgn)
+    print(f'[Date "2003.01.01"]', file=pgn)
+    print(f'[Round "1"]', file=pgn)
+    print(f'[White "{white}"]', file=pgn)
+    print(f'[Black "{black}"]', file=pgn)
+    print(f'[Result "{result}"]', file=pgn)
+    print(f'[Num "{num}"]', file=pgn)
+    print(f'[FEN "{fen}"]', file=pgn)
+    print(f'[Confidence "{confidence:.3f}"]', file=pgn)
+    print(file=pgn)
+    print('*', file=pgn)
+    print(file=pgn)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k", "--book", help="Book author - title")
+    parser.add_argument("results", help="Results file")
+    parser.add_argument("pgn", help="Name of pgn file to write")
+    args = parser.parse_args()
+
+    if args.book:
+        book = args.book
+    else:
+        book = "Unknown"
+
+    white = "N.N"
+    black = "N.N"
+    game_result = "1-0"
+
+    status_BAD = 0
+    skipped = 0
+    with open(args.results, mode='r') as result, open(args.pgn, mode='w') as pgn:
+        for line in result:
+            r = json.loads(line.strip())
+            if r['status'] == "BAD":
+                status_BAD += 1
+                continue
+            num = int(r['file'][:4])
+            confidence = r['confidence']
+            fen = r['fen']
+            try:
+                b = chess.Board(fen)
+                header(book, white, black, game_result, num, confidence, b.fen(), pgn)
+            except:
+                skipped += 1
+
+    print(f"BAD = {status_BAD}")
+    print(f"Skipped = {skipped}")
+   
